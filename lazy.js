feee204d67566aaa7ee9d4673e1f669c85af0928
nrk9819 <nitishranjankalita@gmail.com>
2022-12-26 13:17:18 +0530

    add support for placeholder image

--- lazy.js
+++ lazy.js
@@ -1,27 +1,35 @@
+//Function to create an Intersection Observer
 function createObserver(callback, options){
   const defaultOptions = {
     root: null,
     rootMargin: "0px",
-    threshold: 1
+    threshold: 0.25
   }
   if(!options) return new IntersectionObserver(callback, defaultOptions)
   return new IntersectionObserver(callback, options)
 }
 
+//Function to handle intersection of "image-container" to the root
 function handleImageIntersection(entries, observer){
   entries.forEach(entry => {
     if(entry.isIntersecting) {
       let container = entry.target
-      let image = container.firstElementChild
-      image.src = image.dataset.src
-      image.classList.remove("lazy-image")
+      let placeholder = container.children[0]
+      placeholder.src = placeholder.dataset.src
+      let final = container.children[1]
+      final.src = final.dataset.src
+      final.onload = () => {
+       placeholder.classList.add("hidden")
+       final.classList.remove("hidden")
+      }
+
       observer.unobserve(entry.target)
     }
   })
 }
 
 document.addEventListener("DOMContentLoaded", function () {
-  const imageContainers = document.querySelectorAll(".image-container")
+  const imageContainers = Array.from(document.querySelectorAll(".image-container"))
 
   imageContainers.forEach(imageContainer => {
     if (imageContainer.firstElementChild.currentSrc !== "") imageContainer.classList.remove("placeholder")
@@ -31,9 +39,7 @@ document.addEventListener("DOMContentLoaded", function () {
     let imageObserver = createObserver(handleImageIntersection)
     imageContainers.forEach(async container => {
       imageObserver.observe(container)
-      container.firstElementChild.addEventListener("load", () => container.classList.remove("placeholder"))
+      container.firstElementChild.addEventListener("load", () => container.classList.remove("loader"))
     })
   }
 })
-
-
